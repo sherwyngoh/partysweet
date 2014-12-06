869227c056146c97632ae48474fdc1adaba78d86
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2014-12-06 14:04:04 +0800

    merge

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,37 +3,21 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('EventsCtrl', function($scope, Events) {
+  $scope.events = Events.all();
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+.controller('EventCtrl', function($scope, $stateParams, Events) {
+  $scope.event = Events.get($stateParams.eventId);
 })
 
 .controller('AccountCtrl', function($scope) {
 })
 
-.controller("EventsCtrl", function($scope) {
-	$scope.events = [
-		{id: 0, title: "Party Apocalypso", description: "Amazing party", location: "Clark Quay"},
-		{id: 1, title: "Masquerade Exotica", description: "Mind blowing party", location: "Orchard Road"},
-		{id: 2, title: "Chillennium", description: "Cool blue", location: "M Hotel"},
-		{id: 3, title: "Vodka Tsunami", description: "Whooosh", location: "Carlton Hotel"},
-		{id: 4, title: "House Vibrations", description: "mmmmmmmmmm", location: "Carlton Hotel"},
-	]
-})
-
-.controller("EventCtrl",function($scope) {
-
-})
-
 .controller("newEventCtrl", function($scope){
-	
 })
 
 .controller('ContactsCtrl', function($scope){
-
 })
 
 .controller('PackingListCtrl', function($scope) {
