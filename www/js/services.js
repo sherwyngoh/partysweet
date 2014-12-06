e3d0bfffcc91c602dd437613f51a9a6abc29a3d6
Satrughan <sunnydeveloperacc@gmail.com>
2014-12-06 20:01:55 +0800

    Added fonts

--- www/js/services.js
+++ www/js/services.js
@@ -8,11 +8,11 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var events = [
-    {id: 0, title: "Party Apocalypso", description: "Amazing party", location: "Clark Quay", img: "img/1.jpg"},
-    {id: 1, title: "Masquerade Exotica", description: "Mind blowing party", location: "Orchard Road", img: "img/2.jpg"},
-    {id: 2, title: "Chillennium", description: "Cool blue", location: "M Hotel", img: "img/3.jpg"},
-    {id: 3, title: "Vodka Tsunami", description: "Whooosh", location: "Carlton Hotel", img: "img/1.jpg"},
-    {id: 4, title: "House Vibrations", description: "mmmmmmmmmm", location: "Carlton Hotel", img: "img/4.jpeg"},
+    {id: 0, user_id: 1, title: "Party Apocalypso", description: "Amazing party", location: "Clark Quay", img: "img/1.jpg"},
+    {id: 1, user_id: 2, title: "Masquerade Exotica", description: "Mind blowing party", location: "Orchard Road", img: "img/2.jpg"},
+    {id: 2, user_id: 4, title: "Chillennium", description: "Cool blue", location: "M Hotel", img: "img/3.jpg"},
+    {id: 3, user_id: 5, title: "Vodka Tsunami", description: "Whooosh", location: "Carlton Hotel", img: "img/1.jpg"},
+    {id: 4, user_id: 1, title: "House Vibrations", description: "mmmmmmmmmm", location: "Carlton Hotel", img: "img/4.jpeg"},
   ];
 
   return {
@@ -21,6 +21,26 @@ angular.module('starter.services', [])
     },
     get: function(eventId) {
       return events[eventId];
+    }
+  }
+})
+
+
+.factory('Users', function(){
+  var users = [
+    {id: 0, username: "Sam", user_avatar: "img/profile.jpg"},
+    {id: 1, username: "Sam", user_avatar: "img/profile.jpg"},
+    {id: 2, username: "Sam", user_avatar: "img/profile.jpg"},
+    {id: 3, username: "Sam", user_avatar: "img/profile.jpg"},
+    {id: 4, username: "Sam", user_avatar: "img/profile.jpg"},
+  ]
+
+  return {
+    all: function() {
+      return users;
+    },
+    get: function(userId) {
+      return users[userId];
     }
   }
 })
