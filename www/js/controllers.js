52dd73d193b66caf4b52edf41cf28c6a73aa94d3
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2014-12-07 09:25:36 +0800

    update payment view and need to perform a request with the nonce, not sure what to do with the form button. Added tab functionality for event page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,7 +23,26 @@ angular.module('starter.controllers', [])
 })
 
 .controller('EventCtrl', function($scope, Events, $stateParams, $ionicPopup, $state) {
-    
+  $scope.showDetailsBool = true;
+
+  $scope.showDetails = function(){
+    $scope.showDetailsBool = true;
+    $scope.showMessagesBool = false;
+    $scope.showAttendeesBool = false;
+  }
+
+  $scope.showMessages = function(){
+    $scope.showMessagesBool = true;
+    $scope.showAttendeesBool = false;
+    $scope.showDetailsBool = false;
+  }
+
+  $scope.showAttendees = function(){
+    $scope.showAttendeesBool = true;
+    $scope.showMessagesBool = false;
+    $scope.showDetailsBool = false;
+  }
+
   $scope.goToEvents = function() {
     $state.go('events')
   }
@@ -135,8 +154,8 @@ angular.module('starter.controllers', [])
   //   })
   // },true)
 
-  $scope.saveItems = function(items) {
-    window.localStorage['items'] = JSON.stringify(items)
+  $scope.saveItems = function(items, amountToCharge) {
+    window.localStorage['items'] = JSON.stringify($scope.items || "[]")
     $state.go("summary")
   } 
 
@@ -150,7 +169,16 @@ angular.module('starter.controllers', [])
   
 })
 
-.controller('PaymentCtrl', function($scope, $state) {
-  $scope.braintreeToken = "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiIxYjc0NDI3NjkwZTg0ODZiN2I4MzBkMjMxOWUwZTBjMTE5YTM1MDE3ZjBmNjdjNDFlMjNiODAzZmU0YjUzZjNkfGNyZWF0ZWRfYXQ9MjAxNC0xMi0wNlQyMzoxNjowMi4yNDMyMTUwNjQrMDAwMFx1MDAyNm1lcmNoYW50X2lkPXY2eWh0N25tYzhkNm12dzJcdTAwMjZwdWJsaWNfa2V5PTR5dnRzNnluaDZ2d2JzZ3kiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvdjZ5aHQ3bm1jOGQ2bXZ3Mi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwicGF5bWVudEFwcHMiOltdLCJjbGllbnRBcGlVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvdjZ5aHQ3bm1jOGQ2bXZ3Mi9jbGllbnRfYXBpIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhdXRoVXJsIjoiaHR0cHM6Ly9hdXRoLnZlbm1vLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhbmFseXRpY3MiOnsidXJsIjoiaHR0cHM6Ly9jbGllbnQtYW5hbHl0aWNzLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20ifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6ZmFsc2UsInBheXBhbEVuYWJsZWQiOnRydWUsInBheXBhbCI6eyJkaXNwbGF5TmFtZSI6IlBBUlRZU1dFRVQiLCJjbGllbnRJZCI6bnVsbCwicHJpdmFjeVVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS9wcCIsInVzZXJBZ3JlZW1lbnRVcmwiOiJodHRwOi8vZXhhbXBsZS5jb20vdG9zIiwiYmFzZVVybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9jaGVja291dC5wYXlwYWwuY29tIiwiZGlyZWN0QmFzZVVybCI6bnVsbCwiYWxsb3dIdHRwIjp0cnVlLCJlbnZpcm9ubWVudE5vTmV0d29yayI6dHJ1ZSwiZW52aXJvbm1lbnQiOiJvZmZsaW5lIiwibWVyY2hhbnRBY2NvdW50SWQiOiI3MnRmeXlja3E2bnhkanI1IiwiY3VycmVuY3lJc29Db2RlIjoiVVNEIn0sImNvaW5iYXNlRW5hYmxlZCI6ZmFsc2UsIm1lcmNoYW50SWQiOiJ2NnlodDdubWM4ZDZtdncyIiwidmVubW8iOiJvZmYifQ=="
+.controller('PaymentCtrl', function($scope, $state, $stateParams) {
+  var braintreeToken = "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiIxYjc0NDI3NjkwZTg0ODZiN2I4MzBkMjMxOWUwZTBjMTE5YTM1MDE3ZjBmNjdjNDFlMjNiODAzZmU0YjUzZjNkfGNyZWF0ZWRfYXQ9MjAxNC0xMi0wNlQyMzoxNjowMi4yNDMyMTUwNjQrMDAwMFx1MDAyNm1lcmNoYW50X2lkPXY2eWh0N25tYzhkNm12dzJcdTAwMjZwdWJsaWNfa2V5PTR5dnRzNnluaDZ2d2JzZ3kiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvdjZ5aHQ3bm1jOGQ2bXZ3Mi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwicGF5bWVudEFwcHMiOltdLCJjbGllbnRBcGlVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvdjZ5aHQ3bm1jOGQ2bXZ3Mi9jbGllbnRfYXBpIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhdXRoVXJsIjoiaHR0cHM6Ly9hdXRoLnZlbm1vLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhbmFseXRpY3MiOnsidXJsIjoiaHR0cHM6Ly9jbGllbnQtYW5hbHl0aWNzLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20ifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6ZmFsc2UsInBheXBhbEVuYWJsZWQiOnRydWUsInBheXBhbCI6eyJkaXNwbGF5TmFtZSI6IlBBUlRZU1dFRVQiLCJjbGllbnRJZCI6bnVsbCwicHJpdmFjeVVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS9wcCIsInVzZXJBZ3JlZW1lbnRVcmwiOiJodHRwOi8vZXhhbXBsZS5jb20vdG9zIiwiYmFzZVVybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9jaGVja291dC5wYXlwYWwuY29tIiwiZGlyZWN0QmFzZVVybCI6bnVsbCwiYWxsb3dIdHRwIjp0cnVlLCJlbnZpcm9ubWVudE5vTmV0d29yayI6dHJ1ZSwiZW52aXJvbm1lbnQiOiJvZmZsaW5lIiwibWVyY2hhbnRBY2NvdW50SWQiOiI3MnRmeXlja3E2bnhkanI1IiwiY3VycmVuY3lJc29Db2RlIjoiVVNEIn0sImNvaW5iYXNlRW5hYmxlZCI6ZmFsc2UsIm1lcmNoYW50SWQiOiJ2NnlodDdubWM4ZDZtdncyIiwidmVubW8iOiJvZmYifQ=="
+
+  braintree.setup(braintreeToken, 'dropin', {
+    container: 'dropin',
+    paymentMethodNonceInputField: 'nonce'
+  });
+
+
+  $scope.paymentProcessed = function() {
+  }
 })
 ;
